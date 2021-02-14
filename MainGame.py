2d2c16cab8b885d27a3cc840ffc96069af6204e7
YonSha <yoni.shani448@gmail.com>
2021-02-14 14:03:30 +0200

    phase 2 complete

---
+++ MainGame.py
@@ -0,0 +1,22 @@
+
+    #Yonatan Shani
+from Live import Live
+
+
+
+
+
+
+
+
+def main():
+    ins = Live()
+
+    print(ins.welcome("Guy"))
+    print(ins.load_game())
+  
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ MemoryGame.py
@@ -0,0 +1,49 @@
+from random import randrange
+from time import sleep
+from subprocess import *
+from Game import Game
+
+class MemoryGame(Game):
+
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+
+
+    def generate_sequence(self):
+
+        list_of_nums = []
+        for number in range(0,self.difficulty):
+
+            list_of_nums.append(randrange(1,102))
+
+        return list_of_nums
+
+
+    def get_list_from_user(self):
+
+        user_list = [int(x) for x in input(f"Please enter a size of {self.difficulty} list of numbers: ").split()]
+
+        return user_list
+
+    def is_list_equal(self):
+
+        computer_list = self.generate_sequence()
+        print(computer_list)
+        sleep(0.7)
+        run(["cls"],shell=True)
+
+        user_list = self.get_list_from_user()
+
+        return set(computer_list) == set(user_list)
+
+    def play(self):
+
+
+
+
+       return self.is_list_equal()
+
+
+
+
+
